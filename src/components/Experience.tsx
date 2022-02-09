d6ac907fd191e8a885eb16aecfddd0c7805081ee
Yarex Marvin <yarexmarvin@yahoo.com>
2022-02-09 16:04:13 +0400

    experience panel

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,10 +1,85 @@
+import {
+    Code,
+  Heading,
+  Image,
+  Link,
+  List,
+  ListIcon,
+  ListItem,
+  Tag,
+} from "@chakra-ui/react";
 import { FC } from "react";
+import devstars from "../assets/img/devstars.jpeg";
+import { HiLocationMarker, HiCode } from "react-icons/hi";
+import { MdDateRange } from "react-icons/md";
+import { BsBuilding } from "react-icons/bs";
+import { GoGear } from "react-icons/go";
 
+const Experience: FC = () => {
+  return (
+    <div className="Page-Wrapper Wrapper-Inner">
+      <Heading>Experience</Heading>
 
-const Experience:FC = () => {
-    return <div>
-        Experience
+      <div className="Experience-Wrapper">
+        <div className="Experience__Inner">
+          <div className="Experience__Img">
+            <Link href="https://vk.com/devstars" isExternal>
+              <Image borderRadius={10} src={devstars} boxSize={110} />
+            </Link>
+          </div>
+          <div className="Experience__Description">
+            <List>
+              <ListItem>
+                <ListIcon as={BsBuilding} />
+                Company:{" "}
+                <Link href="https://devstars.team/" isExternal>
+                  <Tag>DevStars</Tag>
+                </Link>
+              </ListItem>
+              <ListItem>
+                <ListIcon as={HiCode} />
+                Role:{" "}
+                <Tag colorScheme="blue" variant="subtle">
+                  Software Developer
+                </Tag>
+              </ListItem>
+              <ListItem>
+                <ListIcon as={MdDateRange} />
+                Period:{" "}
+                <Tag colorScheme="green" variant="outline">
+                  Jul'21 - now
+                </Tag>
+              </ListItem>
+              <ListItem>
+                <ListIcon as={HiLocationMarker} />
+                Location: <Tag variant="solid">Russia, Ulyanovsk</Tag>
+              </ListItem>
+            </List>
+          </div>
+        </div>
+        <div className="Experience__Text">
+          <List>
+          <ListItem>
+              <ListIcon as={GoGear} />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. <Code>Ducimus, sit!</Code> Lorem ipsum dolor. Lorem, ipsum.
+            </ListItem>
+            <ListItem>
+              <ListIcon color="whatsapp.300" as={GoGear} />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, sit! Lorem, ipsum dolor.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={GoGear} />
+              Lorem ipsum dolor sit, <Code> amet consectetur </Code>  adipisicing elit. Ducimus, sit! Lorem ipsum dolor sit amet.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={GoGear} />
+              Lorem <Code>ipsum dolor </Code> sit, amet consectetur adipisicing elit. Ducimus, sit!
+            </ListItem>
+          </List>
+        </div>
+      </div>
     </div>
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,6 +7,7 @@ import {
   List,
   ListIcon,
   ListItem,
+  Tag,
   Text,
 } from "@chakra-ui/react";
 import { FC } from "react";
@@ -36,6 +37,7 @@ import {
   SiFigma,
   SiSass,
 } from "react-icons/si";
+import { GoBrowser } from "react-icons/go";
 import { AiFillGitlab, AiFillCode, AiOutlineCode } from "react-icons/ai";
 import { FaGitAlt, FaSass } from "react-icons/fa";
 import { FiFigma } from "react-icons/fi";
@@ -53,70 +55,123 @@ const Skills: FC = () => {
         connected to the server.
       </Text>
 
+      <List className="Skills__Environiment">
+        <ListItem>
+          <Heading size="sm">
+            <ListIcon as={GoBrowser} />
+            for browser environment:
+          </Heading>
+          <Kbd>
+            Desktop <ListIcon as={BsDisplay} />{" "}
+          </Kbd>
+          <Kbd>
+            Tablet <ListIcon as={BsTabletLandscape} />
+          </Kbd>
+          <Kbd>
+            Mobile <ListIcon as={BsPhone} />
+          </Kbd>
+        </ListItem>
+      </List>
+
       <div className="Skills">
         <Heading size="lg">Technologies:</Heading>
         <List className="Skills__List">
           <ListItem>
             <ListIcon as={AiFillCode} />
-           for Desktop <ListIcon as={BsDisplay} /> for Tablet{" "}
-            <ListIcon as={BsTabletLandscape} /> for Mobile{" "}
-            <ListIcon as={BsPhone} />
+            <Code colorScheme="red">
+              HTML5 <ListIcon as={SiHtml5} />{" "}
+            </Code>
+            <Code colorScheme="twitter">
+              CSS3 <ListIcon as={SiCss3} />
+            </Code>
           </ListItem>
 
           <ListItem>
             <ListIcon as={AiFillCode} />
-            HTML5 <ListIcon as={SiHtml5} /> CSS3 <ListIcon as={SiCss3} />
-            JavaScript(ES12) <ListIcon as={SiJavascript} /> TypeScript
-            <ListIcon as={SiTypescript} />
+            <Code colorScheme="yellow">
+              JavaScript <ListIcon as={SiJavascript} />
+            </Code>
+            <Code colorScheme="blue">
+              TypeScript <ListIcon as={SiTypescript} />
+            </Code>
           </ListItem>
 
           <ListItem>
             <ListIcon as={AiFillCode} />
-            React <ListIcon as={SiReact} /> React-Router-Dom{" "}
-            <ListIcon as={SiReactrouter} />
+            <Code colorScheme="telegram">
+              React <ListIcon as={SiReact} />
+            </Code>
+            React-Router-Dom <ListIcon as={SiReactrouter} />
           </ListItem>
           <ListItem className="ListItem-VK">
             <ListIcon as={AiFillCode} />
-            VK API <ListIcon as={Icon16LogoVk} /> VKUI VK Bridge
+            <Code bgColor="#0077ff">
+              VK API <ListIcon as={Icon16LogoVk} />
+            </Code>
+            VKUI VK Bridge
             <ListItem as={Icon24LogoVkOutline} />
           </ListItem>
           <ListItem>
             <ListIcon as={AiFillCode} />
-            HTTP REST API Axios <ListIcon as={MdHttp} />
+            REST API <ListIcon as={MdHttp} />{" "}
+            <Code bgColor="#5a29e4">Axios</Code>
           </ListItem>
           <ListItem>
             <ListIcon as={AiFillCode} />
-            <ListIcon as={SiRedux} />
-            Redux Redux-Toolkit
+            <Code colorScheme="purple">
+              <ListIcon as={SiRedux} />
+              Redux
+            </Code>
+            Redux-Toolkit
           </ListItem>
           <ListItem>
             <ListIcon as={AiFillCode} />
-            RTK Query Redux-Thunk Redux-Saga <ListIcon as={SiReduxsaga} />
+            Redux-Thunk{" "}
+            <Code colorScheme="rgb(134, 212, 107)">
+              Redux-Saga <ListIcon as={SiReduxsaga} />
+            </Code>
           </ListItem>
           <ListItem>
-          <ListIcon as={AiFillCode} />
+            <ListIcon as={AiFillCode} />
             <Code colorScheme="orange">
               MobX <ListIcon as={SiMobx} />
-              </Code>
+            </Code>
           </ListItem>
           <ListItem>
             <ListIcon as={AiFillCode} />
-            Jest <ListIcon as={SiJest} /> React-Testing-Library{" "}
-            <ListIcon as={SiTestinglibrary} />
+            <Code bgColor="rgb(21, 194, 19)" color="#FFF">
+              Jest <ListIcon as={SiJest} />
+            </Code>
+            <Code color="red">
+              Testing-Library <ListIcon as={SiTestinglibrary} />
+            </Code>
           </ListItem>
           <ListItem>
             <ListIcon as={AiFillCode} />
-            NPM <ListIcon as={DiNpm} /> Yarn <ListIcon as={SiYarn} />
+            <Tag variant='solid' bgColor="red">
+              <ListIcon as={DiNpm} /> NPM 
+            </Tag>
+            <Code bgColor="#2188b6">
+              Yarn <ListIcon as={SiYarn} />
+            </Code>
           </ListItem>
           <ListItem>
             <ListIcon as={AiFillCode} />
-            <ListIcon as={SiWebpack} />
-            Webpack{" "}
+            <Code colorScheme="blue">
+              <ListIcon as={SiWebpack} /> Webpack
+            </Code>
           </ListItem>
           <ListItem>
             <ListIcon as={AiFillCode} />
-            Git <ListIcon as={FaGitAlt} /> GitHub <ListIcon as={SiGithub} />
-            GitLab <ListIcon as={AiFillGitlab} />
+            <Code color="#f14e32">
+              Git <ListIcon as={FaGitAlt} />
+            </Code>
+            <Code color="white" colorScheme="blackAlpha">
+              GitHub <ListIcon as={SiGithub} />
+            </Code>
+            <Tag variant='subtle' color="#f14e32">
+              GitLab <ListIcon as={AiFillGitlab} />
+            </Tag>
           </ListItem>
           <ListItem>
             <ListIcon as={AiFillCode} />
@@ -130,26 +185,40 @@ const Skills: FC = () => {
             <ListItem>
               {" "}
               <ListIcon as={AiOutlineCode} />
-              React-Native React-Navigation{" "}
+              <Code color="#06bcee">React-Native</Code>
+              React-Navigation
             </ListItem>
             <ListItem>
               <ListIcon as={AiOutlineCode} />
               <ListIcon as={SiApollographql} />
-              Apollo Client <ListIcon as={SiGraphql} />
-              GraphQL
+              Apollo Client
+              <Code color="#e10098">
+                <ListIcon as={SiGraphql} />
+                GraphQL
+              </Code>
             </ListItem>
             <ListItem>
               <ListIcon as={AiOutlineCode} />
-              SASS/SCSS <ListIcon as={SiSass} /> BEM GSAP
+              <Code bgColor="#bf4080">
+                SASS/SCSS <ListIcon as={SiSass} />
+              </Code>
+              BEM GSAP
             </ListItem>
             <ListItem>
               <ListIcon as={AiOutlineCode} />
-              Next.js <ListIcon as={SiNextdotjs} /> React-Query
+              Next.js <ListIcon as={SiNextdotjs} />
+              <Code bgColor="rgb(255, 65, 84)">React-Query</Code>
+              RTK-Query
             </ListItem>
             <ListItem>
               <ListIcon as={AiOutlineCode} />
-              Prettier <ListIcon as={SiPrettier} /> ESLint{" "}
-              <ListIcon as={SiEslint} />
+              <Code bgColor="#1a2b34">
+                Prettier <ListIcon as={SiPrettier} />
+              </Code>
+              <Code bgColor="#3a33d1">
+                ESLint
+                <ListIcon as={SiEslint} />
+              </Code>
             </ListItem>
           </List>
         </div>
